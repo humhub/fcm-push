98e3d1c84587448747165d6675e5889f215fca58
HumHub Translations <hello@humhub.com>
2023-03-28 08:42:48 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/ko/base.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '<strong>FireBase Messaging</strong> Configuration' => '',
+    'API Key' => '',
+    'Cloud Messaging API (Legacy)' => '',
+    'Install ID' => '',
+    'Sender ID' => '',
+    'Service Account (JSON file)' => '',
+];
