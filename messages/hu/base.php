c6684a1aa6f620fcc90d54b5ce162b68213899df
HumHub Translations <hello@humhub.com>
2023-04-28 18:26:05 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    '<strong>FireBase Messaging</strong> Configuration' => '',
-    'API Key' => '',
-    'Cloud Messaging API (Legacy)' => '',
-    'Install ID' => '',
-    'Sender ID' => '',
-    'Service Account (JSON file)' => '',
-];
+return array (
+  '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase Messaging</strong> konfiguráció',
+  'API Key' => 'API Kulcs',
+  'Cloud Messaging API (Legacy)' => 'Cloud Messaging API (örökölt)',
+  'Install ID' => 'Telepítési azonosító',
+  'Sender ID' => 'Feladó azonosítója',
+  'Service Account (JSON file)' => 'Szolgáltatásfiók (JSON-fájl)',
+);
