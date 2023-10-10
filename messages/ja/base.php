79d89e540de67556dbacc56f5ece9ec612fe755d
HumHub Translations <hello@humhub.com>
2023-10-10 07:10:31 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -1,16 +1,15 @@
 <?php
-
-return [
-    '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase メッセージング</strong> 構成',
-    'API Key' => 'API Key',
-    'Cloud Messaging API (Legacy)' => 'クラウドメッセージングAPI(Legacy)',
-    'Install ID' => 'Install ID',
-    'Sender ID' => '送信者 ID',
-    'Service Account (JSON file)' => 'サービスアカウント(JSON File)',
-    'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => '',
-    'Firebase Cloud Messaging (required for browser & PWA notifications)' => '',
-    'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => '',
-    'Link Redirection Service' => '',
-    'Push Service (required for the mobile app) (Beta)' => '',
-    'To send Firebase push notifications with your own Firebase project, enter your access details here.' => '',
-];
+return array (
+  '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase メッセージング</strong> 構成',
+  'API Key' => 'API Key',
+  'Cloud Messaging API (Legacy)' => 'クラウドメッセージングAPI(Legacy)',
+  'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'リンクリダイレクト サービスを有効にします。リンクをモバイルブラウザーではなくモバイルデバイスのアプリで開くには、すべてのリンク (通知メールなど) を HumHub プロキシ サーバー経由でルーティングする必要があります。 (実験的な機能 // <a href="{url}">プライバシー ポリシー</a>)',
+  'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Firebase Cloud Messaging (ブラウザと PWA 通知に必要)',
+  'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'HumHub モバイル アプリ ユーザーの場合、HumHub プッシュ サービス経由でプッシュ通知を送信できます。このサービスを使用したい場合は、以下にアクセス キーを入力してください。<br>このプッシュ ゲートウェイは、公式 HumHub モバイル アプリのユーザーのみが利用できることに注意してください。',
+  'Install ID' => 'Install ID',
+  'Link Redirection Service' => 'リンクリダイレクトサービス',
+  'Push Service (required for the mobile app) (Beta)' => 'プッシュ サービス (モバイル アプリに必要) (ベータ版)',
+  'Sender ID' => '送信者 ID',
+  'Service Account (JSON file)' => 'サービスアカウント(JSON File)',
+  'To send Firebase push notifications with your own Firebase project, enter your access details here.' => '独自の Firebase プロジェクトで Firebase プッシュ通知を送信するには、ここにアクセスの詳細を入力します。',
+);
