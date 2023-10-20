40b39354a588c6438073b4b289310318cb15c229
HumHub Translations <hello@humhub.com>
2023-10-20 18:16:41 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,16 +1,15 @@
 <?php
-
-return [
-    '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase Messaging</strong> konfiguráció',
-    'API Key' => 'API Kulcs',
-    'Cloud Messaging API (Legacy)' => 'Cloud Messaging API (örökölt)',
-    'Install ID' => 'Telepítési azonosító',
-    'Sender ID' => 'Feladó azonosítója',
-    'Service Account (JSON file)' => 'Szolgáltatásfiók (JSON-fájl)',
-    'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => '',
-    'Firebase Cloud Messaging (required for browser & PWA notifications)' => '',
-    'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => '',
-    'Link Redirection Service' => '',
-    'Push Service (required for the mobile app) (Beta)' => '',
-    'To send Firebase push notifications with your own Firebase project, enter your access details here.' => '',
-];
+return array (
+  '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase Messaging</strong> konfiguráció',
+  'API Key' => 'API Kulcs',
+  'Cloud Messaging API (Legacy)' => 'Cloud Messaging API (örökölt)',
+  'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'Link-átirányítási szolgáltatás engedélyezése. Ahhoz, hogy a linkek a mobileszközökön az alkalmazásban nyíljanak meg, nem pedig a mobilböngészőben, minden hivatkozást (például értesítő e-maileket) a HumHub proxyszerveren keresztül kell irányítani. (Kísérleti funkciók // <a href="{url}">Adatvédelmi irányelvek</a>)',
+  'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Firebase Cloud Messaging (szükséges a böngésző és a PWA értesítésekhez)',
+  'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'A HumHub mobilalkalmazás felhasználói a push értesítéseket a HumHub push szolgáltatáson keresztül küldhetik el. Ha használni szeretné ezt a szolgáltatást, kérjük, adja meg hozzáférési kulcsát alább.<br>Kérjük, vegye figyelembe, hogy ez a push átjáró csak a hivatalos HumHub mobilalkalmazás felhasználói számára érhető el.',
+  'Install ID' => 'Telepítési azonosító',
+  'Link Redirection Service' => 'Link átirányítási szolgáltatás',
+  'Push Service (required for the mobile app) (Beta)' => 'Push szolgáltatás (a mobilalkalmazáshoz szükséges) (béta)',
+  'Sender ID' => 'Feladó azonosítója',
+  'Service Account (JSON file)' => 'Szolgáltatásfiók (JSON-fájl)',
+  'To send Firebase push notifications with your own Firebase project, enter your access details here.' => 'Ha Firebase push értesítéseket szeretne küldeni saját Firebase-projektjével, adja meg itt a hozzáférési adatait.',
+);
