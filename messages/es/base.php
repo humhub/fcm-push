18b41b36ba490f7bba562d6faccf83c2293c4534
HumHub Translations <hello@humhub.com>
2024-01-24 19:31:22 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,16 +1,15 @@
 <?php
-
-return [
-    'API Key' => 'Clave de la API',
-    '<strong>FireBase Messaging</strong> Configuration' => '',
-    'Cloud Messaging API (Legacy)' => '',
-    'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => '',
-    'Firebase Cloud Messaging (required for browser & PWA notifications)' => '',
-    'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => '',
-    'Install ID' => '',
-    'Link Redirection Service' => '',
-    'Push Service (required for the mobile app) (Beta)' => '',
-    'Sender ID' => '',
-    'Service Account (JSON file)' => '',
-    'To send Firebase push notifications with your own Firebase project, enter your access details here.' => '',
-];
+return array (
+  '<strong>FireBase Messaging</strong> Configuration' => 'Configuración de la <strong>Mensajería FireBase</strong>',
+  'API Key' => 'Clave de la API',
+  'Cloud Messaging API (Legacy)' => 'API de la mensajería en la nube (Antigua)',
+  'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'Activar el Servicio de Redirección de Enlaces. Para que estos enlaces se abran en la aplicación para dispositivos móviles en lugar de en el navegador móvil, todos los enlaces (por ejemplo los correos de notificación) deben ser gestionados por el servidor proxy de Humhub. (Funciones experimentales // <a href="{url}">Política de privacidad</a>)',
+  'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Mensajería Firebase en la Nube (requerida para notificaciones en el navegador y en PWA)',
+  'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'Para los usuarios de aplicaciones móviles Humhub, las notificaciones push pueden enviarse a través del servicio push de Humhub. Si quiere utilizar este servicio, por favor introduzca debajo su clave de acceso.<br>Por favor tenga en cuenta que esta pasarela push está disponible únicamente para los usuarios de la aplicación móvil Humhub oficial.',
+  'Install ID' => 'ID de instalación',
+  'Link Redirection Service' => 'Servicio de Redirección de Enlaces',
+  'Push Service (required for the mobile app) (Beta)' => 'Servicio Push (necesario para la aplicación móvil) (Beta)',
+  'Sender ID' => 'ID de remitente',
+  'Service Account (JSON file)' => 'Cuenta de Servicio (fichero JSON)',
+  'To send Firebase push notifications with your own Firebase project, enter your access details here.' => 'Para enviar notificaciones push de Firebase con tu propio proyecto de Firebase, introduzca aquí sus credenciales de acceso.',
+);
