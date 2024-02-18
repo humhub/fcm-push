10d24d8405222e1c779d7e0bb854c8d25a319a92
HumHub Translations <hello@humhub.com>
2024-02-18 18:19:32 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -1,15 +1,18 @@
 <?php
-return array (
-  '<strong>FireBase Messaging</strong> Configuration' => 'Configuration de <strong>FireBase Messaging</strong>',
-  'API Key' => 'Clé d’API',
-  'Cloud Messaging API (Legacy)' => 'API de messagerie cloud (Legacy)',
-  'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'Activer le service de redirection de liens. Afin que les liens s\'ouvrent dans l\'application sur les appareils mobiles, plutôt que dans le navigateur mobile, tous les liens (par exemple, les e-mails de notification) doivent être acheminés via le serveur proxy HumHub. (Fonctionnalités expérimentales // <a href="{url}">Politique de confidentialité</a>)',
-  'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Firebase Cloud Messaging (nécessaire pour les notifications du navigateur et des applications Web progressives)',
-  'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'Pour les utilisateurs de l\'application mobile HumHub, les notifications push peuvent être envoyées via le service de push HumHub. Si vous souhaitez utiliser ce service, veuillez entrer votre clé d\'accès ci-dessous. Veuillez noter que cette passerelle de push n\'est disponible que pour les utilisateurs de l\'application mobile officielle HumHub.',
-  'Install ID' => 'ID d\'installation',
-  'Link Redirection Service' => 'Service de redirection de liens',
-  'Push Service (required for the mobile app) (Beta)' => 'Service de notifications push (nécessaire pour l\'application mobile) (version bêta)',
-  'Sender ID' => 'ID de l\'expéditeur',
-  'Service Account (JSON file)' => 'Compte de service (fichier JSON)',
-  'To send Firebase push notifications with your own Firebase project, enter your access details here.' => 'Pour envoyer des notifications push Firebase avec votre propre projet Firebase, saisissez ici vos informations d\'accès.',
-);
+
+return [
+    '<strong>FireBase Messaging</strong> Configuration' => 'Configuration de <strong>FireBase Messaging</strong>',
+    'API Key' => 'Clé d’API',
+    'Cloud Messaging API (Legacy)' => 'API de messagerie cloud (Legacy)',
+    'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'Activer le service de redirection de liens. Afin que les liens s\'ouvrent dans l\'application sur les appareils mobiles, plutôt que dans le navigateur mobile, tous les liens (par exemple, les e-mails de notification) doivent être acheminés via le serveur proxy HumHub. (Fonctionnalités expérimentales // <a href="{url}">Politique de confidentialité</a>)',
+    'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Firebase Cloud Messaging (nécessaire pour les notifications du navigateur et des applications Web progressives)',
+    'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'Pour les utilisateurs de l\'application mobile HumHub, les notifications push peuvent être envoyées via le service de push HumHub. Si vous souhaitez utiliser ce service, veuillez entrer votre clé d\'accès ci-dessous. Veuillez noter que cette passerelle de push n\'est disponible que pour les utilisateurs de l\'application mobile officielle HumHub.',
+    'Install ID' => 'ID d\'installation',
+    'Link Redirection Service' => 'Service de redirection de liens',
+    'Push Service (required for the mobile app) (Beta)' => 'Service de notifications push (nécessaire pour l\'application mobile) (version bêta)',
+    'Sender ID' => 'ID de l\'expéditeur',
+    'Service Account (JSON file)' => 'Compte de service (fichier JSON)',
+    'To send Firebase push notifications with your own Firebase project, enter your access details here.' => 'Pour envoyer des notifications push Firebase avec votre propre projet Firebase, saisissez ici vos informations d\'accès.',
+    'Disable AuthChoices on iOS App' => '',
+    'Hide third-party login options for app users with iOS.' => '',
+];
