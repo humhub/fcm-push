788434cb2ea98dafa787610739495139e91569b8
HumHub Translations <hello@humhub.com>
2024-02-23 19:29:16 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,18 +1,17 @@
 <?php
-
-return [
-    '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase Messaging</strong>-configuratie',
-    'API Key' => 'API sleutel',
-    'Cloud Messaging API (Legacy)' => 'Cloud Messaging-API (verouderd)',
-    'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'Schakel de link-omleidingsservice in. Om ervoor te zorgen dat links in de app op mobiele apparaten worden geopend, in plaats van in de mobiele browser, moeten alle links (bijvoorbeeld notificatie-e-mails) via de HumHub-proxyserver worden geleid. (Experimentele functies // <a href="{url}">Privacybeleid</a>)',
-    'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Firebase Cloud Messaging (vereist voor browser- en PWA-meldingen)',
-    'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'Voor gebruikers van de HumHub mobiele app kunnen pushmeldingen worden verzonden via de HumHub-pushservice. Als je deze service wilt gebruiken, voer dan hieronder je toegangssleutel in.<br>Let op: deze push-gateway is alleen beschikbaar voor gebruikers van de officiële HumHub mobiele app.',
-    'Install ID' => 'Installatie ID',
-    'Link Redirection Service' => 'Link-omleidingsservice',
-    'Push Service (required for the mobile app) (Beta)' => 'Pushservice (vereist voor de mobiele app) (bèta)',
-    'Sender ID' => 'Zender ID',
-    'Service Account (JSON file)' => 'Serviceaccount (JSON-bestand)',
-    'To send Firebase push notifications with your own Firebase project, enter your access details here.' => 'Voer uw toegangsgegevens in om Firebase-pushmeldingen te verzenden in uw eigen Firebase-project.',
-    'Disable AuthChoices on iOS App' => '',
-    'Hide third-party login options for app users with iOS.' => '',
-];
+return array (
+  '<strong>FireBase Messaging</strong> Configuration' => '<strong>FireBase Messaging</strong>-configuratie',
+  'API Key' => 'API sleutel',
+  'Cloud Messaging API (Legacy)' => 'Cloud Messaging-API (verouderd)',
+  'Disable AuthChoices on iOS App' => 'Schakel AuthChoices uit in de iOS-app',
+  'Enable Link Redirection Service. In order for links to open in the app on mobile devices, rather than in the mobile browser, all links (e.g. notification emails) need to be routed through the HumHub proxy server. (Experimental Features // <a href="{url}">Privacy Policy</a>)' => 'Schakel de link-omleidingsservice in. Om ervoor te zorgen dat links in de app op mobiele apparaten worden geopend, in plaats van in de mobiele browser, moeten alle links (bijvoorbeeld notificatie-e-mails) via de HumHub-proxyserver worden geleid. (Experimentele functies // <a href="{url}">Privacybeleid</a>)',
+  'Firebase Cloud Messaging (required for browser & PWA notifications)' => 'Firebase Cloud Messaging (vereist voor browser- en PWA-meldingen)',
+  'For HumHub mobile app users, push notifications can be sent via the HumHub push service. If you want to use this service, please enter your access key below.<br/>Please note, this push gateway is only available for the users of the official HumHub mobile app.' => 'Voor gebruikers van de HumHub mobiele app kunnen pushmeldingen worden verzonden via de HumHub-pushservice. Als je deze service wilt gebruiken, voer dan hieronder je toegangssleutel in.<br>Let op: deze push-gateway is alleen beschikbaar voor gebruikers van de officiële HumHub mobiele app.',
+  'Hide third-party login options for app users with iOS.' => 'Verberg inlogopties van derden voor app-gebruikers met iOS.',
+  'Install ID' => 'Installatie ID',
+  'Link Redirection Service' => 'Link-omleidingsservice',
+  'Push Service (required for the mobile app) (Beta)' => 'Pushservice (vereist voor de mobiele app) (bèta)',
+  'Sender ID' => 'Zender ID',
+  'Service Account (JSON file)' => 'Serviceaccount (JSON-bestand)',
+  'To send Firebase push notifications with your own Firebase project, enter your access details here.' => 'Voer uw toegangsgegevens in om Firebase-pushmeldingen te verzenden in uw eigen Firebase-project.',
+);
