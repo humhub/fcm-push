a10b0921518003e989c6d9866df165f956d335d8
Marc Farré <contact@marc.fun>
2024-12-13 12:13:14 +0000

    Fix redirection after switching instance

--- controllers/MobileAppController.php
+++ controllers/MobileAppController.php
@@ -3,19 +3,22 @@
 namespace humhub\modules\fcmPush\controllers;
 
 use humhub\components\Controller;
+use humhub\libs\Html;
 use humhub\modules\fcmPush\helpers\MobileAppHelper;
 use Yii;
 
 class MobileAppController extends Controller
 {
-
     public function actionInstanceOpener()
     {
         // Send to the mobile app to display the instance opener
         Yii::$app->session->set(MobileAppHelper::SESSION_VAR_SHOW_OPENER, 1);
 
-        // Stay in the same page, for when we come back from the mobile app to this instance
-        return $this->refresh();
+        // Stay on the same page, because when we come back from the mobile app to this instance
+        // Force full page refresh to trigger the onLayoutAddonInit event
+        return $this->renderContent(sprintf(
+            '<script ' . Html::nonce() . '>window.location.href = "%s";</script>',
+            Yii::$app->request->referrer,
+        ));
     }
-
 }
