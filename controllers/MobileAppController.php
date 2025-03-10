e4c639e4a7094d942decc8efb0cfa1a579d20319
Marc Farré <contact@marc.fun>
2025-03-10 14:27:56 +0000

    fix

--- controllers/MobileAppController.php
+++ controllers/MobileAppController.php
@@ -4,6 +4,7 @@ namespace humhub\modules\fcmPush\controllers;
 
 use humhub\components\Controller;
 use humhub\modules\fcmPush\helpers\MobileAppHelper;
+use Yii;
 use yii\helpers\Url;
 
 class MobileAppController extends Controller
@@ -11,8 +12,7 @@ class MobileAppController extends Controller
     public function actionInstanceOpener()
     {
         MobileAppHelper::registerShowOpenerScript();
-
-        // Stay on the same page, because when we come back from the mobile app to this instance
-        return $this->redirect(Url::home());
+        Yii::$app->view->registerJs('window.location.href = "' . Url::home() . '";');
+        return $this->renderContent('');
     }
 }
